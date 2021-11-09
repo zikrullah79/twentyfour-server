10d711086e39d8036c22292ee496a712f1b5ab6e
zikrullah79 <zikrullahaliwaindah@gmail.com>
2021-11-09 17:46:16 +0700

    initialize room and player socket system

---
+++ model/Player.go
@@ -0,0 +1,66 @@
+package model
+
+import (
+	"bytes"
+	"log"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type Player struct {
+	Id    uint
+	State int
+	Point float64
+	Conn  *websocket.Conn
+	Room  *Room
+	Send  chan []byte
+}
+
+func (u *Player) ReadPlayerUpdate() {
+	for {
+		_, msg, err := u.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		u.Room.Broadcast <- bytes.TrimSpace(msg)
+	}
+}
+
+func (u *Player) WritePlayerUpdate() {
+	ticker := time.NewTicker(60 * time.Second)
+	defer func() {
+		ticker.Stop()
+		u.Conn.Close()
+	}()
+	for {
+		select {
+		case msg, ok := <-u.Send:
+			if !ok {
+				u.Conn.NextWriter(websocket.CloseMessage)
+				return
+			}
+			w, err := u.Conn.NextWriter(websocket.TextMessage)
+
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			w.Write(msg)
+
+			que := len(u.Send)
+			for i := 0; i < que; i++ {
+				w.Write([]byte("/n"))
+				w.Write(<-u.Send)
+			}
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := u.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
---
+++ model/Room.go
@@ -0,0 +1,50 @@
+package model
+
+import (
+	"math/rand"
+)
+
+type Room struct {
+	Id         uint
+	Status     int
+	Players    map[*Player]bool
+	Broadcast  chan []byte
+	Register   chan *Player
+	Unregister chan *Player
+}
+
+func newRoom() *Room {
+	return &Room{
+		Id:         uint(rand.Uint64()),
+		Status:     0,
+		Players:    make(map[*Player]bool),
+		Broadcast:  make(chan []byte),
+		Register:   make(chan *Player),
+		Unregister: make(chan *Player),
+	}
+}
+
+func (r *Room) Run() {
+	for {
+		select {
+		case player := <-r.Register:
+			r.Players[player] = true
+		case player := <-r.Unregister:
+			if _, ok := r.Players[player]; ok {
+				delete(r.Players, player)
+				close(player.Send)
+			}
+
+		case logplay := <-r.Broadcast:
+			for player := range r.Players {
+				select {
+				case player.Send <- logplay:
+				default:
+					close(player.Send)
+					delete(r.Players, player)
+				}
+
+			}
+		}
+	}
+}
